229b3bdc791ca5ac4c485ae8860234ab02dc8110
Natasha Chernis <natashachernis@gmail.com>
2019-04-08 03:39:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,13 +19,15 @@ class Song
   end
 
   def self.new_by_name(song_name)
-    song = self.create
+    song = self.new
     song.name = song_name
     song
   end
 
   def self.create_by_name(song_name)
-    self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    self.all << song
     song
   end
 
